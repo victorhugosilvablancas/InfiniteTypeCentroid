9a08607312779281ac8bcbbaca8d0e77f4ecb4fc
Victor Hugo Silva <102427408+victorhugosilvablancas@users.noreply.github.com>
2025-03-19 17:47:46 -0600

    Delete apidocs directory

--- apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"uaq.dcc.datasets"},{"l":"uaq.dcc.definitions"},{"l":"uaq.dcc.ploteo"},{"l":"uaq.infinitetypecentroid"}];updateSearchResults();
